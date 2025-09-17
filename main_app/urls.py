da5b3ed623199d99201901a6c959557f0fd96cc9
guyera <alec@graymansurvivaltech.com>
2025-09-17 14:57:34 -0400

    Added the ProjectGroup model

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
+    path("project-groups/", views.ProjectGroupList.as_view(), name="project-groups-index"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from .models import ProjectGroup
+from django.views.generic import ListView
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("<h1>Welcome to our new app!</h1>")
+    return render(request, "home.html")
 
 
 def about(request):
     return render(request, "about.html")
+
+class ProjectGroupList(ListView):
+    model = ProjectGroup
